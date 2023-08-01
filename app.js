bead12583d049abb9be0c8e650f256f5784a3f04
AntanasMac <118132764+AntanasMac@users.noreply.github.com>
2023-08-01 17:26:09 +0300

    qs

--- app.js
+++ app.js
@@ -41,3 +41,20 @@ function courierCalculator(dienosPajamos,dienosIslaidos,nuvaziuotasAtstumas,atli
 
   let dienosPajamosInput = 49.36;
   let PaskaiciavimoRezultatas = courierCalculator(dienosPajamosInput,15,83.9,12,6);
+
+  // formos submit --> sudejimas i object
+
+  document.getElementById("forma").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent the default form submission behavior
+
+    // Get form input values
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+
+    // Create an object with the form data
+    const formData = {
+      name: name,
+      email: email,
+      message: message
+    };
