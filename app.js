f21c86589480af1324e11d9204c0a383ee601bbc
AntanasMac <118132764+AntanasMac@users.noreply.github.com>
2023-08-02 00:19:09 +0300

    qs

--- app.js
+++ app.js
@@ -62,30 +62,6 @@ function courierCalculator(dienosPajamos,dienosIslaidos,nuvaziuotasAtstumas,atli
   //     komentaras: komentaras,
   //   };
 
-// menu box funckionalumas
-
-
-  
-  
-  const dailyReport = document.getElementById('dailyReport');
-  let DailyReportClickCounter = false;
-
-
- dailyReport.addEventListener('click', function clicker(){
-  
-  if (DailyReportClickCounter === false){
-  const calculatorBoxCSS= document.querySelector('#CBelement');
-  calculatorBoxCSS.style.display = 'block';
-  console.log('calculator box visible');
-  DailyReportClickCounter = true;} 
-  
-  else if(DailyReportClickCounter === true){
-  const calculatorBoxCSS= document.querySelector('#CBelement');
-  calculatorBoxCSS.style.display = 'none';
-  console.log('calculator box visible');
-  return DailyReportClickCounter = false;
- }});
-
 
 //  if (calculatorBoxCSS.style.display = 'block'){
 //   calculatorBoxCSS.style.display = 'none';
